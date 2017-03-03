388ba2390e0e50a0f04fc74075aafd03ed1636d3
jayantiayu <jayantiayu4@gmail.com>
2017-03-03 07:29:48 -0800

    CodeIgniter
    Framework_Praktikum1

---
+++ CodeIgniter/application/controllers/Baru.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Baru extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Baru.php */
+/* Location: ./application/controllers/Baru.php */
---
+++ CodeIgniter/application/views/about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>My Blogs</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand">My Blog</a>
+					</div>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="<?php echo site_url()?>/home">Home</a></li>
+							<li><a href="<?php echo site_url()?>/about">About</a></li>
+							<li><a href="<?php echo site_url()?>/contact">Contact</a></li>
+							
+						</ul>
+						
+						<ul class="nav navbar-nav navbar-right">
+							
+							<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Administrator <b class="caret"></b></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										
+									</ul>
+								</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</nav>
+			<div align="justify"  class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			<h1>About Me</h1>
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+
+
+				Nama&nbsp&nbsp&nbsp: Jayanti Ayu Saktifasari
+				<br>
+				<br>
+				Nim&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: 1541180229
+				<br>
+				<br>
+				Kelas&nbsp&nbsp&nbsp&nbsp: TI-2B
+				<br>
+				<br>
+				Alamat&nbsp: Puri Cempaka Putih 2 Malang
+				<br>
+				<br>
+				No.Hp&nbsp&nbsp: 08970424307
+				<br>
+				<br>
+				Hobi&nbsp&nbsp&nbsp&nbsp&nbsp: Bermain Musik
+				<br>
+				<br>
+
+			<table class="table table-hover">
+							<thead>
+								<tr>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+			</div>
+		</div>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="<?php echo base_url("")?>assets/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
